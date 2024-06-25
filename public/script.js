fc76c4b435e1a8a6681ca4c44a1e1ec805ad5771
mhbb8897 <mahbub.ahwani89@gmail.com>
2024-06-25 11:13:58 +0700

    Fix Audio

--- public/script.js
+++ public/script.js
@@ -6,19 +6,3 @@ window.onload = () => {
         document.getElementById('heading').textContent = query;
     }
 };
-document.addEventListener('DOMContentLoaded', function () {
-    var audio = document.getElementById('myAudio');
-    var playPromise = audio.play();
-
-    if (playPromise !== undefined) {
-        playPromise.then(function () {
-            console.log('Audio is playing automatically');
-        }).catch(function (error) {
-            console.log('Audio autoplay was prevented');
-            // Wait for user interaction
-            document.body.addEventListener('click', function () {
-                audio.play();
-            }, { once: true });
-        });
-    }
-});
